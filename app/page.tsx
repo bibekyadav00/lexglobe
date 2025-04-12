1708e25bfd30136608e633dbbc81282962b9d405
Bibek Yadav <bibekyadav00@gmail.com>
2025-04-12 18:46:31 +0530

    c1

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default async function Home() {
             <Button asChild size="lg" className="bg-white text-primary hover:bg-gray-100">
               <Link href="/contact">Contact Us</Link>
             </Button>
-            <Button asChild size="lg" variant="outline" className="border-white text-white hover:bg-white/10">
+            <Button asChild size="lg" variant="outline" className="border-white text-black hover:bg-white/10">
               <Link href="/practice-areas">Our Services</Link>
             </Button>
           </div>
