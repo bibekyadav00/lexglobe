8b959cf9b0359d1c34f2ed425cf7fbce130896b3
Bibek Yadav <bibekyadav00@gmail.com>
2025-04-14 12:51:58 +0530

    c19

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,23 +10,23 @@ import SocialButtons from "@/components/social-buttons"
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from '@vercel/speed-insights/next';
  
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <head>
-        <title>Next.js</title>
-      </head>
-      <body>
-        {children}
-        <SpeedInsights />
-      </body>
-    </html>
-  );
-}
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <head>
+//         <title>Next.js</title>
+//       </head>
+//       <body>
+//         {children}
+//         <SpeedInsights />
+//       </body>
+//     </html>
+//   );
+// }
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
