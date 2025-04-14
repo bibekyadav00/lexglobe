41c6ff5a896992b3a24a553cee0a053cc0ea4263
Bibek Yadav <bibekyadav00@gmail.com>
2025-04-14 12:32:01 +0530

    c15

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import Header from "@/components/header"
 import Footer from "@/components/footer"
 import SocialButtons from "@/components/social-buttons"
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
